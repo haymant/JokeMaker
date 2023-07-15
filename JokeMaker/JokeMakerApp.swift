046efebb5d76838a99e13c3b47bc228c10f9dd6b
haymant <dev@lizhao.net>
2023-07-15 16:53:59 +0800

    Initial Commit

---
+++ JokeMaker/JokeMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeMakerApp.swift
+//  JokeMaker
+//
+//  Created by Li, Zhao on 15/7/23.
+//
+
+import SwiftUI
+
+@main
+struct JokeMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JokeMakerUITests/JokeMakerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JokeMakerUITestsLaunchTests.swift
+//  JokeMakerUITests
+//
+//  Created by Li, Zhao on 15/7/23.
+//
+
+import XCTest
+
+final class JokeMakerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
